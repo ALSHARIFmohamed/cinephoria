4173dd9071c8ee3ce6ee46d24d026132a0603394
ALSHARIF <mohamed@MacBook-Pro-de-Mohamed.local>
2025-05-22 06:27:51 +0200

    Mise à jour complète avant déploiement

--- cinephoria_backend/settings.py
+++ cinephoria_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 AUTH_USER_MODEL = 'users.CustomUser'
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'cinephoria_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-import dj_database_url
+
 
 DATABASES = {
     # 'default': {
@@ -102,8 +102,8 @@ DATABASES = {
     )
 }
 
-
-
+DATABASES['default']= dj_database_url.parse("postgresql://cinephoria_db_user:M6AhCfTCJMmHT29xrEMO3is315MghYqq@dpg-d0n9e2gdl3ps73a0l15g-a.oregon-postgres.render.com/cinephoria_db")
+#postgresql://cinephoria_db_user:M6AhCfTCJMmHT29xrEMO3is315MghYqq@dpg-d0n9e2gdl3ps73a0l15g-a.oregon-postgres.render.com/cinephoria_db
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
